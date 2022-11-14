55a481441cbb9bf433a73f398a352c2542ffc940
doak <doakcovington@protonmail.com>
2022-11-14 14:21:15 -0800

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+// WDGTR
+// NCWH
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,16 +10,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Howdy!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
